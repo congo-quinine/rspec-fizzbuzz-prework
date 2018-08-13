b8df3aa6bf76ceb9b4616ad65bae7efee793a520
Rob Parker <congo.quinine@gmail.com>
2018-08-13 16:08:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,3 +2,5 @@
     if num % 3 == 0 && num % 5 == 0
       return "FizzBuzz"
   end
+end
+SyntaxError: /home/congo-quinine/rspec-fizzbuzz-prework/fizzbuzz.rb:4: syntax error, unexpected end-of-input, expecting keyword_end

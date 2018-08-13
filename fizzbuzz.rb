f561d7ec916ebda4eece72a6dc36d0477156657a
Rob Parker <congo.quinine@gmail.com>
2018-08-13 16:20:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@
     if num % 3 == 0 && num % 5 == 0
       return "FizzBuzz"
     elsif num % 5 == 0
-      return "Fizz"
-    elsif num % 3 == 0
       return "Buzz"
+    elsif num % 3 == 0
+      return "Fizz"
     elsif  !(num % 3 == 0 || num % 5 == 0)
       return nil
   end
